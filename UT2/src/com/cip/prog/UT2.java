6c5e0536c6ea41b4d040b089ad34c525af3476a8
neogladium <neogladium@gmail.com>
2020-02-06 22:13:47 +0000

    Commit inicial

---
+++ UT2/src/com/cip/prog/UT2.java
@@ -0,0 +1,17 @@
+package com.cip.prog;
+
+public class UT2 {
+
+	public UT2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi nombre es Sergio");
+		System.out.println("Tengo 43 años");
+		System.out.println("Estudio programación en el módulo DAM");
+
+	}
+
+}
